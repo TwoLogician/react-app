2f86b43f13533e8f87ed6452e77ce372670009a8
The Amazing Developer-Man <Two.Logician@icloud.com>
2018-07-18 20:33:05 +0700

    :sparkles: axios FormData

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
+import * as axios from "axios"
 import * as React from "react"
 import * as NumberFormat from "react-number-format"
 
 import "./App.css"
 import logo from "./logo.svg"
 
+
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  fileChange = e => {
+    let data = new FormData()
+    data.append("files", e.target.files[0])
+    data.append("json", JSON.stringify({ roomId: 1, userId: 1 }))
+    axios.default.post("http://localhost:5001/api/folders/import", data).then(rs => {
+      console.log(rs)
+      let url = `http://localhost:5001/api/files?fileName=${rs.data}`
+      let link = document.createElement("a")
+      link.href = url
+      link.click()
+      window.URL.revokeObjectURL(url);
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
   public render() {
+
     return (
       <div className="App">
         <header className="App-header">
@@ -16,6 +39,9 @@ class App extends React.Component {
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
         <NumberFormat thousandSeparator={true} prefix={'$'} />
+        <div>
+          <input onChange={this.fileChange} type="file" />
+        </div>
       </div>
     )
   }
